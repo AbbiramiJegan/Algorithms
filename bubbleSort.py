79a0b0ed8f1ba94a02d9506142bd5e42389e87f0
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2024-04-03 15:33:33 +0800

    Changed the swap method

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,20 +1,20 @@
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(my_list):
+    n = len(my_list)
     # Perform n - 1 passes
     for i in range(n - 1):
         # Initialize flag to False for each pass
         flag = False
         # In each pass, compare each element with its adjacent element
         for j in range(n - i - 1):
-            if arr[j] > arr[j + 1]:
+            if my_list[j] > my_list[j + 1]:
                 # Swap the elements
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
                 # Set flag to True if a swap occurs
                 flag = True
-        # If no swaps occur in a pass, the array is already sorted, so break the loop
+        # If no swaps occur in a pass, the my_list is already sorted, so break the loop
         if not flag:
             break
 
-arr = [8, 8, 3, 5, 4]
-bubble_sort(arr)
-print("Sorted array:", arr)
+my_list = [8, 8, 3, 5, 4]
+bubble_sort(my_list)
+print("Sorted array:", my_list)
