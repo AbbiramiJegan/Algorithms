e4a973712520da3db2006e3364a21c857d9c7db4
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2024-05-06 12:20:19 +0800

    Selection sort in python completed

--- selectionSort.py
+++ selectionSort.py
@@ -1,7 +1,6 @@
 def selection_sort(my_list):
     for i in range(len(my_list)):
         minimum = i 
-        print(minimum)
         for j in range(i+1, len(my_list)):
             if my_list[minimum] > my_list[j]:
                 minimum = j
